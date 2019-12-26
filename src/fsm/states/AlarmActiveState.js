18b6696210cdffbf9a7fc1015043f374c8e2a895
Husak Yurii <gusak.1881@gmail.com>
2019-12-26 13:36:08 +0200

    Alarm active state was updated

--- src/fsm/states/AlarmActiveState.js
+++ src/fsm/states/AlarmActiveState.js
@@ -2,7 +2,25 @@ import IState from "./IState";
 
 export default class AlarmActiveState extends IState {
 
-    onEnterState() { }
+    onEnterState() { 
+        const { controller } = this.fsm.clock;
+        const { view: { buttons } } = controller;
 
-    onExitState(cb) { }
+        controller.enableButtons();
+        buttons.forEach(({ _, element }) =>   element.onclick = (event) => this.onButtonClick(event));
+    }
+
+    onButtonClick(event) {
+        this.fsm.gotToIdleState();
+    }
+
+    onExitState(cb) { 
+        const { controller } = this.fsm.clock;
+        const { view: { buttons } } = controller;
+
+        controller.disableAlarm();
+
+        buttons.forEach(({ _, element }) => element.onclick = null);
+        cb();
+    }
 }
