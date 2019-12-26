3d764c669329fcf3108f34b8ffc13ab1580d706a
Husak Yurii <gusak.1881@gmail.com>
2019-12-26 13:42:06 +0200

    index file was refactored to remove unised variables

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import Clock from "./clock/Clock";
+
 const clock = new Clock();
 clock.initialize();
 clock.run();
 
-let oldTime = performance.now();
-const tiker = function (newTime = performance.now()) {
-
-	let delta = Number((newTime - oldTime).toFixed(2));
-	oldTime = newTime;
-
-	clock.update(delta);
+const tiker = function () {
+	clock.update();
 	window.requestAnimationFrame(tiker);	
 };
 
 tiker();
+
+window.clock = clock;
