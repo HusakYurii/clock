a8746f18507348b5e4d75ddf2f5db02de1c20de2
Husak Yurii <gusak.1881@gmail.com>
2019-12-26 12:41:02 +0200

    AlarmIdleState controller was updated

--- src/fsm/states/AlarmIdleState.js
+++ src/fsm/states/AlarmIdleState.js
@@ -2,7 +2,38 @@ import IState from "./IState";
 
 export default class AlarmIdleState extends IState {
 
-    onEnterState() { }
+    onEnterState() {
+        const { controller } = this.fsm.clock;
+        const { view: { buttons } } = controller;
 
-    onExitState(cb) { }
+        controller.enableButtons();
+        controller.showAlarmTime();
+
+        buttons.forEach(({ name, element }) => {
+            element.onclick = (event) => this.onButtonClick(event, name);
+        });
+    }
+
+    onButtonClick(event, name) {
+        console.log(name);
+        switch (name) {
+            case "changeHours":
+                this.fsm.clock.controller.changeHours();
+                break;
+            case "changeMinutes":
+                this.fsm.clock.controller.changeMinutes();
+                break;
+            default:
+                this.fsm.gotToIdleState();
+                break;
+        }
+    }
+
+    onExitState(cb) {
+        const { controller } = this.fsm.clock;
+        const { view: { buttons } } = controller;
+
+        buttons.forEach(({ _, element }) => element.onclick = null);
+        cb();
+    }
 }
